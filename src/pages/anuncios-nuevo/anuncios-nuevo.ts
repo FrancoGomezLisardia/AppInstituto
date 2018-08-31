820d36a5bdb1e71e1cb31d163ddc9f4549738235
franco <francogl91@hotmail.com>
2018-08-31 14:27:00 -0300

    31/08/2018_3

--- src/pages/anuncios-nuevo/anuncios-nuevo.ts
+++ src/pages/anuncios-nuevo/anuncios-nuevo.ts
@@ -60,6 +60,25 @@ export class AnunciosNuevoPage {
 
 
   }
+
+// mostrar_camara(){
+//   const options: CameraOptions = {
+//     quality: 50,
+//     destinationType: this.camera.DestinationType.DATA_URL,
+//     encodingType: this.camera.EncodingType.JPEG,
+//     mediaType: this.camera.MediaType.PICTURE
+//   }
+
+//   this.camera.getPicture(options).then((imageData) => {
+//    // imageData is either a base64 encoded string or a file URI
+//    // If it's base64:
+//    this.imagenPreview = 'data:image/jpeg;base64,' + imageData;
+//   this.imagen64=imageData;
+//   }, (err) => {
+//    // Handle error
+//    console.log("Error en camara",JSON.stringify(err))
+//   });
+// }
   registrar(){
     let archivo={
       img:this.imagen64,
